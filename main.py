8d57062f21b58486ebb04279c7d21667ce7010ab
misslinabaloona <takoren3822@gmail.com>
2024-11-29 03:04:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+#1st programme
+print ((9 ** 0.5) * 5)
+
+#2nd programme
+print (9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd programme (1 вариант)
+result1 = (2 * 2 or + 2)
+result2 = (2 * 2 and + 2)
+print (result1 == result2)
+#3rd programme (2 вариант)
+result3 = (2 * 2 + 2)
+result4 = (2 * (2 + 2))
+print (result3 == result4)
+#до конца не поняла что имелось ввиду под "приоритетом", но конечные результаты правильные :D
+
+#4th programme
+(type(float('123.456')))
+number = (float('123.456'))
+number1 = (number * 10)
+number2 = int(number1)
+number3 = (number2 // 10)
+number4 = (number3 // 10)
+number5 = int(number4 / 3)
+print (number5)
+
