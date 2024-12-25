fd8f93b250eebc0a3e835c3aa551287e88bb1a0d
MissLunaBaloona <takoren3822@gmail.com>
2024-12-25 04:01:42 +0400

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -1,1 +1,9 @@
-print('hello')
+first = int(input("Введите целое число №1: "))
+second = int(input("Введите целое число №2: "))
+third = int(input("Введите целое число №3: "))
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
